0c70ba899d7a5d1af9fac71e409a77a2ba9c9f9c
ranjan0247 <ranjanmamadapur@gmail.com>
2026-08-28 15:18:08 +0530

    Updated java code

--- largestno.java
+++ largestno.java
@@ -4,14 +4,12 @@ public class largestno {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 1. Take input from the user
         System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
 
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
 
-        // FIXED: Capitalized "System"
         System.out.print("Enter third number: "); 
         int num3 = scanner.nextInt();
         
